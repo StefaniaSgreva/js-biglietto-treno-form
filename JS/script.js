8d6d42dcef2a56d8a3ee467419627958eaa7d09e
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-07 15:43:19 +0200

    starter point

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+"use strict"; 
+
+const calcBtn = document.getElementById('calc');
+
+const ticketPriceCalc = function(){
+    let km = document.getElementById('km').value;
+    let age = document.getElementById('age').value;
+    console.log(km);
+    console.log(age);
+
+
+}
