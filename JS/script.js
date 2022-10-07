bfcec7ec7e7d47d7f02f52c99c8fea3cc05bb5b7
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-07 16:22:32 +0200

    added parseInt

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,8 @@
 const btn = document.getElementById('calculate');
 
 const CalcTicketPrice = function (){
-    let km = document.getElementById('km').value; 
-    let age = document.getElementById('age').value; 
+    let km = parseInt(document.getElementById('km').value); 
+    let age = parseInt(document.getElementById('age').value); 
     console.log(km, age);
 
 
