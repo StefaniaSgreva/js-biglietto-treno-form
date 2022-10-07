f4558102a57f86eeb73ce6dae3938c409a758ee7
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-07 16:19:47 +0200

    starter point debugged

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,15 @@
 "use strict"; 
 
-const calcBtn = document.getElementById('calc');
+const btn = document.getElementById('calculate');
 
-const ticketPriceCalc = function(){
-    let km = document.getElementById('km').value;
-    let age = document.getElementById('age').value;
-    console.log(km);
-    console.log(age);
+const CalcTicketPrice = function (){
+    let km = document.getElementById('km').value; 
+    let age = document.getElementById('age').value; 
+    console.log(km, age);
+
+
 
 
 }
+
+btn.addEventListener('click', CalcTicketPrice);
