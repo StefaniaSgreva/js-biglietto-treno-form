34b1bb899e461cdf177c4f3ff6017af9bddbc928
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-10-07 16:43:02 +0200

    added function

--- JS/script.js
+++ JS/script.js
@@ -7,8 +7,30 @@ const CalcTicketPrice = function (){
     let age = parseInt(document.getElementById('age').value); 
     console.log(km, age);
 
+    const costPerKm = 0.21;
+    let ticketCost = costPerKm * km;
+    console.log(ticketCost);
 
+    const underage = ticketCost / 100 * 80;
+    const over = ticketCost / 100 * 60;
 
+    console.log(underage, over);
+
+    let correctPriceAdult = ticketCost.toFixed(2);
+    let correctPriceUnderage = underage.toFixed(2);
+    let correctPriceOver = over.toFixed(2);
+    console.log(correctPriceAdult, correctPriceUnderage, correctPriceOver);  
+
+    if ((age >= 18) && (age <= 65)){
+        console.log(correctPriceAdult);
+        // const element = document.getElementById('ticket-price').innerHTML = correctPriceAdult;
+    } else if (age < 18){
+        console.log(correctPriceUnderage);
+        // const element = document.getElementById('ticket-price').innerHTML = correctPriceUnderage += '<div style="color: #b31b1a; font-size: 16px;">Hai ricevuto uno sconto del 20%</div>';
+    } else if (age > 65) {
+        console.log(correctPriceOver);
+        // const element = document.getElementById('ticket-price').innerHTML = correctPriceOver += '<div style="color: #b31b1a; font-size: 16px;">Hai ricevuto uno sconto del 40%</div>';
+    }
 
 }
 
